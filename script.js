49526eaf4336335f997c593937c660676e2c9b40
Mookgo Lebaka <138899392+Mookgol@users.noreply.github.com>
2024-01-12 14:44:19 +0200

    added

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  
+  function submitForm() {
+    // You can add your form submission logic here
+    // For now, let's just log the form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    // You can replace the above console.log statements with your actual form submission code
+    // For example, you might use AJAX to send the form data to a server
+}
